15e6f1d07c637a54122dd8febe12064c0bb0a559
Eik Chen <eik@eikde-macbook-pro.local>
2017-09-26 14:56:30 +0800

    Initial Commit

---
+++ CocoaExam1/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CocoaExam1
+//
+//  Created by Eik Chen on 2017/9/26.
+//  Copyright © 2017年 Eik Chen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CocoaExam1/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CocoaExam1
+//
+//  Created by Eik Chen on 2017/9/26.
+//  Copyright © 2017年 Eik Chen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
