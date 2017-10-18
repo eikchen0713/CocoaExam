de1e82b9c43a1566101cd3b5c04d12774437b804
Eik Chen <eik@Eikde-MacBook-Pro.local>
2017-10-18 14:54:45 +0800

    first commit

---
+++ CocoaExam1/SecView.swift
@@ -0,0 +1,18 @@
+//
+//  SecView.swift
+//  CocoaExam1
+//
+//  Created by Eik Chen on 2017/10/16.
+//  Copyright © 2017年 Eik Chen. All rights reserved.
+//
+
+import Cocoa
+
+class SecView: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
